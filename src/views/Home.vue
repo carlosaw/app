1daa01baf7a0b24b2b919765f1dc52bfefff786c
carlosaw <carlosfreevg@gmail.com>
2020-11-30 21:51:43 -0400

    First Commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-button variant="success">
+      Hello Word
+    </b-button>
   </div>
 </template>
 
