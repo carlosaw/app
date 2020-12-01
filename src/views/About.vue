f16a4c34f0f8a7a139607f873f377160fc1e880a
carlosaw <carlosfreevg@gmail.com>
2020-11-30 22:10:09 -0400

    TableBootstrapVue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,40 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <b-table :items="items"></b-table>
   </div>
   
 </template>
+
+<script>
+export default {
+  data(){
+    return{
+      items:[
+        {
+          id:1,
+          name:'Carlos',
+          telefone: 36868374,
+          ações:''
+        },
+        {
+          id:2,
+          name:'Mara',
+          telefone: 36828874,
+          ações:''
+        },
+        {
+          id:3,
+          name:'Ana Paula',
+          telefone: 36848974,
+          ações:''
+        }
+      ]
+    }
+  }
+}
+</script>
+<style>
+  .about {
+    border-bottom:1px solid red
+  }
+</style>
